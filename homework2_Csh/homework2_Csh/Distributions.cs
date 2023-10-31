bbf01c1992d132059d0f7a433ebbf6694e9ad708
SimoneCiferri <73128699+SimoneCiferri@users.noreply.github.com>
2023-10-31 22:29:09 +0100

    homework2_Csh update 2

---
+++ homework2_Csh/homework2_Csh/Distributions.cs
@@ -0,0 +1,379 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2_Csh
+{
+    internal class Distributions
+    {
+        //This function extracts the values of the i-column from the .csv file with relative path " relativePathpath " and returns it as a List of strings
+        public List<String> extractValuesFromCSV(string relativePath, int i)
+        {
+            using (StreamReader reader1 = new StreamReader(relativePath))
+            {
+                List<String> col_values = new List<String>();
+                List<String> row;
+                string line;
+                while ((line = reader1.ReadLine()) != null)
+                {
+                    row = ParseCsvLine(line);
+
+                    if (!((row[i] == "") || (row[i] == "-")))
+                    {
+                        col_values.Add(row[i]);
+                    }
+                }
+                col_values.RemoveAt(0);
+                return col_values;
+            }
+        }
+
+        //This function is to perform the row split avoiding errors on multivalues (in .csv files the " " are used to represents multivalues in a cell)
+        public List<string> ParseCsvLine(string line)
+        {
+            List<string> values = new List<string>();
+            bool inQuotes = false;
+            int start = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+
+                if (c == '"')
+                {
+                    inQuotes = !inQuotes;
+                }
+                else if (c == ',' && !inQuotes)
+                {
+                    string value = line.Substring(start, i - start);
+                    values.Add(value.Trim('"'));
+                    start = i + 1;
+                }
+            }
+
+            // add last value after last " , "
+            string lastValue = line.Substring(start);
+            values.Add(lastValue.Trim('"'));
+
+            return values;
+        }
+
+        
+        //this function creates a distribution from input data "col" (list of strings)  and returns it as a Dictionary variable
+        public Dictionary<string, int> createDistrib(List<string> col)
+        {
+            Dictionary<string, int> distrib = new Dictionary<string, int>();
+
+            foreach (string val in col)
+            {
+                // if exists inc. counter
+                if (distrib.ContainsKey(val))
+                {
+                    distrib[val]++;
+                }
+                else // if not then add and set counter = 1
+                {
+                    distrib[val] = 1;
+                }
+            }
+            return distrib;
+        }
+
+        
+        //this function creates a distribution from input data "col" (continuous values), calculates the intervals and returns it as a Dictionary variable
+        public Dictionary<string, int> createDistribFromContinuous(List<string> col, int k)
+        {
+            Dictionary<string, int> distrib = new Dictionary<string, int>();
+
+
+            // In this point I'll ask user to insert k (in this Windows form application i choose manually the k but i can simply parse
+            // it with:
+            // int k = int.Parse(Console.ReadLine()) from the console);
+
+            // Convert String to Double to calculate intervalls
+            List<double> double_col = new List<double>();
+
+            foreach (string value in col)
+            {
+                
+                if (double.TryParse(value, out double doubleValue))
+                {
+                    double_col.Add(doubleValue);
+                    Debug.WriteLine($"STO STAMPANN O VALOR '{value}'");
+                }
+                else
+                {
+                    Debug.WriteLine($"The value '{value}' is not valid (It will be ignored).");
+                }
+            }
+            double_col.Sort();
+
+            // Calculate the lenght of each intervall
+            double intervallenght = (double_col.Max() - double_col.Min()) / k;
+            Debug.WriteLine($"INTERVAL LEN '{intervallenght}'");
+            Debug.WriteLine($"INTERVAL Max '{double_col.Max()}'");
+            Debug.WriteLine($"INTERVAL Min '{double_col.Min()}'");
+            Debug.WriteLine($"INTERVAL Min '{double_col.Min()}'");
+
+            //calculate distribution
+            foreach (double val in double_col)
+            {
+
+                if (val != 0)
+                {
+                    int interval = (int)((val - double_col.Min()) / intervallenght)+1;
+                    if (interval >= 0)
+                    {
+                        string string_interval;
+                        if (interval <= k) {
+                            string_interval = interval.ToString();
+                        }
+                        else {
+                            string_interval = (interval-1).ToString();
+                        }
+                        Debug.WriteLine($"VALUE '{val}' is on intervall '{string_interval}'");
+                        // if exists inc. counter
+                        if (distrib.ContainsKey(string_interval))
+                        {
+                            distrib[string_interval]++;
+                        }
+                        else // if not then add and set counter = 1
+                        {
+                            distrib[string_interval] = 1;
+                        }
+
+                    }
+                }
+
+                /*
+                int interval = (int)((val - double_col.Min()) / intervallenght) + 1;
+                string string_interval = interval.ToString();
+
+                // if exists inc. counter
+                if (distrib.ContainsKey(string_interval))
+                {
+                    distrib[string_interval]++;
+                }
+                else // if not then add and set counter = 1
+                {
+                    distrib[string_interval] = 1;
+                }
+                */
+            }
+
+            return distrib;
+        }
+
+        //This function calculates the absolute frequency distribution of " inputDistrib "
+        public Dictionary<string, double> absFrequencyDistrib(Dictionary<string, int> inputDistrib)
+        {
+            Dictionary<string, double> absFreqDistrib = new Dictionary<string, double>();
+
+            int totalCateg = 0;
+            foreach (var coppia in inputDistrib)
+            {
+                totalCateg += coppia.Value;
+            }
+
+            // Calculate all freq. distrib.
+            foreach (var coppia in inputDistrib)
+            {
+                string cat = coppia.Key;
+
+                //absolute
+                int absoluteFreq = coppia.Value;
+
+                absFreqDistrib.Add(cat, absoluteFreq);
+
+            }
+            return absFreqDistrib;
+        }
+
+        //This function calculates the relative frequency distribution of " inputDistrib "
+        public Dictionary<string, double> relativeFrequencyDistrib(Dictionary<string, int> inputDistrib)
+        {
+            Dictionary<string, double> relFreqDistrib = new Dictionary<string, double>();
+
+            int totalCateg = 0;
+            foreach (var coppia in inputDistrib)
+            {
+                totalCateg += coppia.Value;
+            }
+
+            // Calculate all rel. freq. distrib.
+            foreach (var coppia in inputDistrib)
+            {
+                string cat = coppia.Key;
+
+                //absolute
+                int absoluteFreq = coppia.Value;
+                //relative
+                double relativeFrequency = (double)absoluteFreq / totalCateg;
+
+                relFreqDistrib.Add(cat, relativeFrequency);
+
+            }
+            return relFreqDistrib;
+        }
+
+        //This function calculates the percentage frequency distribution of " inputDistrib "
+        public Dictionary<string, double> percFrequencyDistrib(Dictionary<string, int> inputDistrib)
+        {
+            Dictionary<string, double> percFreqDistrib = new Dictionary<string, double>();
+
+            int totalCateg = 0;
+            foreach (var coppia in inputDistrib)
+            {
+                totalCateg += coppia.Value;
+            }
+
+            // Calculate all perc. freq. distrib.
+            foreach (var coppia in inputDistrib)
+            {
+                string cat = coppia.Key;
+
+                //absolute
+                int absoluteFreq = coppia.Value;
+                //relative
+                double relativeFrequency = (double)absoluteFreq / totalCateg;
+                //percentage
+                double percentageFrequency = relativeFrequency * 100;
+
+                percFreqDistrib.Add(cat, percentageFrequency);
+
+            }
+            return percFreqDistrib;
+        }
+
+        //This function calculare the multivariate distribution of variables specified in the variables input list (in that list the
+        // user has to specify the columns of the variables) 
+        public Dictionary<string[], int> calculateMultivariateDistrib(String relativePath, List<int> variables)
+        {
+            //multivariable distribution will be a Dictionary variable with a list of string as a key. Because of that i
+            // need to define implement the class ArrayComparer that compare two different list of strings
+            // (otherwise the .ContainsKey method of the dictionary doesn't work)
+            //
+            Dictionary<string[], int> multivariateDistribution = new Dictionary<string[], int>(new ArrayComparer());
+
+            using (StreamReader reader1 = new StreamReader(relativePath))
+            {
+                List<String> row;
+                string line;
+                int variables_number = variables.Count;
+                bool first = true;
+
+                //Start parsing
+                while ((line = reader1.ReadLine()) != null)
+                {
+                    //this if is to delete the columns names (on .csv file the first row parsed is exactly the columns names)
+                    if (first)
+                    {
+                        first = false;
+                        continue;
+                    }
+
+                    row = ParseCsvLine(line);
+
+                    //calculate distribution
+                    if (variables.All(colIndex => colIndex < row.Count))
+                    {
+                        string[] selectedValues = variables.Select(colIndex => row[colIndex]).ToArray();
+                        if (multivariateDistribution.ContainsKey(selectedValues))
+                        {
+                            multivariateDistribution[selectedValues]++;
+                        }
+                        else
+                        {
+                            multivariateDistribution[selectedValues] = 1;
+                        }
+                    }
+
+                }
+            }
+            return multivariateDistribution;
+
+        }
+
+        //This function calculates the absolute frequency distribution of multivariate input distrib
+        public Dictionary<string[], double> multiAbsFrequencyDistrib(Dictionary<string[], int> multivariateDistribution)
+        {
+            Dictionary<string[], double> absFreqDistrib = new Dictionary<string[], double>();
+
+            // Calculate total count
+            int totalCount = multivariateDistribution.Values.Sum();
+
+            //calculate absolute distribution
+            foreach (var pair in multivariateDistribution)
+            {
+                // Absolute frequency
+                int absoluteFrequency = pair.Value;
+
+                absFreqDistrib.Add(pair.Key, absoluteFrequency);
+            }
+            return absFreqDistrib;
+        }
+
+        //This function calculates the relative frequency distribution of multivariate input distrib
+        public Dictionary<string[], double> multiRelativeFrequencyDistrib(Dictionary<string[], int> multivariateDistribution)
+        {
+            Dictionary<string[], double> relFreqDistrib = new Dictionary<string[], double>();
+
+            // Calculate total count
+            int totalCount = multivariateDistribution.Values.Sum();
+
+            //calculate absolute distribution
+            foreach (var pair in multivariateDistribution)
+            {
+                // Absolute frequency
+                int absoluteFrequency = pair.Value;
+                // Relative frequency
+                double relativeFrequency = (double)absoluteFrequency / totalCount;
+
+                relFreqDistrib.Add(pair.Key, relativeFrequency);
+            }
+            return relFreqDistrib;
+        }
+
+        //This function calculates the percentage frequency distribution of multivariate input distrib
+        public Dictionary<string[], double> multiPercFrequencyDistrib(Dictionary<string[], int> multivariateDistribution)
+        {
+            Dictionary<string[], double> percFreqDistrib = new Dictionary<string[], double>();
+
+            // Calculate total count
+            int totalCount = multivariateDistribution.Values.Sum();
+
+            //calculate absolute distribution
+            foreach (var pair in multivariateDistribution)
+            {
+                // Absolute frequency
+                int absoluteFrequency = pair.Value;
+                // Relative frequency
+                double relativeFrequency = (double)absoluteFrequency / totalCount;
+                // Percentage frequency
+                double percentageFrequency = relativeFrequency * 100;
+
+                percFreqDistrib.Add(pair.Key, percentageFrequency);
+            }
+            return percFreqDistrib;
+        }
+
+        // Comparer for arrays, used to compare arrays of strings with .ContainsKet dictionary method (For Multivariate Distribution)
+        public class ArrayComparer : IEqualityComparer<string[]>
+        {
+            public bool Equals(string[] x, string[] y)
+            {
+                return x.SequenceEqual(y);
+            }
+
+            public int GetHashCode(string[] obj)
+            {
+                return obj.Aggregate(0, (current, val) => current ^ val.GetHashCode());
+            }
+        }
+    }
+}
---
+++ homework2_Csh/homework2_Csh/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework2_Csh
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //retrieve relative document paths
+            String currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            String relativePath = Directory.GetParent(currentDirectory).Parent.Parent.FullName;
+            String afterLifeSourveyPath = System.IO.Path.Combine(relativePath, "SurveyAfterLife-Sheet1.csv");
+            String profLifeSourveyPath = System.IO.Path.Combine(relativePath, "ProfessionalLife-Sheet1.csv");
+
+            //def dictionary to save distributions
+            Dictionary<string, int> backgroundDistrib = new Dictionary<string, int>();
+            Dictionary<string, int> remunerationDistrib = new Dictionary<string, int>();
+            Dictionary<string, int> hardWorkerDistrib = new Dictionary<string, int>();
+            Dictionary<string, double> absFreqDistrib = new Dictionary<string, double>();
+            Dictionary<string, double> relFreqDistrib = new Dictionary<string, double>();
+            Dictionary<string, double> percFreqDistrib = new Dictionary<string, double>();
+            Dictionary<string[], int> multivariateDistribution = new Dictionary<string[], int>(new Distributions.ArrayComparer());
+            Dictionary<string[], double> multiAbsFreqDistrib = new Dictionary<string[], double>();
+            Dictionary<string[], double> multiRelFreqDistrib = new Dictionary<string[], double>();
+            Dictionary<string[], double> multiPercFreqDistrib = new Dictionary<string[], double>();
+
+            //Use the class Distribution
+            Distributions distribution = new Distributions();
+
+            try
+            {
+                //retrieve values from .cvs file
+                List<String> background_col = distribution.extractValuesFromCSV(profLifeSourveyPath, 1);
+                List<String> remuneration_col = distribution.extractValuesFromCSV(profLifeSourveyPath, 6);
+                List<String> hardWorker_col = distribution.extractValuesFromCSV(profLifeSourveyPath, 4);
+
+                //create single distributions
+                backgroundDistrib = distribution.createDistrib(background_col);
+
+                int k = 2;
+                remunerationDistrib = distribution.createDistribFromContinuous(remuneration_col, k);
+
+                hardWorkerDistrib = distribution.createDistrib(hardWorker_col);
+
+
+                //calculate frequency distributions
+                absFreqDistrib = distribution.absFrequencyDistrib(backgroundDistrib);
+                relFreqDistrib = distribution.relativeFrequencyDistrib(backgroundDistrib);
+                percFreqDistrib = distribution.percFrequencyDistrib(backgroundDistrib);
+                printAllFrequencyDistributions(absFreqDistrib, relFreqDistrib, percFreqDistrib);
+
+                absFreqDistrib = distribution.absFrequencyDistrib(remunerationDistrib);
+                relFreqDistrib = distribution.relativeFrequencyDistrib(remunerationDistrib);
+                percFreqDistrib = distribution.percFrequencyDistrib(remunerationDistrib);
+                printAllFrequencyDistributions(absFreqDistrib, relFreqDistrib, percFreqDistrib);
+
+                absFreqDistrib = distribution.absFrequencyDistrib(hardWorkerDistrib);
+                relFreqDistrib = distribution.relativeFrequencyDistrib(hardWorkerDistrib);
+                percFreqDistrib = distribution.percFrequencyDistrib(hardWorkerDistrib);
+                printAllFrequencyDistributions(absFreqDistrib, relFreqDistrib, percFreqDistrib);
+
+                //calculate Bivariate distribution
+                //first specify the columns
+                List<int> variables = new List<int>();
+                variables.Add(4);
+                variables.Add(16);
+                //if you add the following line of code you will calculate Multivariate distribution of columns 4, 16 and 2
+                //variables.Add(2);
+                multivariateDistribution = distribution.calculateMultivariateDistrib(profLifeSourveyPath, variables);
+                multiAbsFreqDistrib = distribution.multiAbsFrequencyDistrib(multivariateDistribution);
+                multiRelFreqDistrib = distribution.multiRelativeFrequencyDistrib(multivariateDistribution);
+                multiPercFreqDistrib = distribution.multiPercFrequencyDistrib(multivariateDistribution);
+                printAllMultivariateFrequencyDistributions(multiAbsFreqDistrib, multiRelFreqDistrib, multiPercFreqDistrib);
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Exception thrown: " + ex.Message;
+            }
+
+        }
+
+
+        //print all frequency distributions
+        private void printAllFrequencyDistributions(Dictionary<string, double> absoluteFreq, Dictionary<string, double> relativeFreq, Dictionary<string, double> percFreq) {
+
+            //From here I start printing resuslts inside a label on the Windows Form
+            Debug.WriteLine("Category      |      absolute      |      Relative      |      Percentage%      |");
+            labelCat.Text += ("\n\nCATEGORY");
+            labelAbs.Text += ("\n\nABSOLUTE");
+            labelRel.Text += ("\n\nRELATIVE");
+            labelPerc.Text += ("\n\nPERCENTAGE");
+
+            // Print all distrib.
+            for (int i = 0; i < absoluteFreq.Keys.Count; i++)
+            {
+                Debug.WriteLine(message: $"({absoluteFreq.Keys.ElementAt(i),-10})      ({absoluteFreq.Values.ElementAt(i),-18})      {relativeFreq.Values.ElementAt(i),-18:F4}      {percFreq.Values.ElementAt(i),-18:F2}%");
+                labelCat.Text += ("\n" + absoluteFreq.Keys.ElementAt(i));
+                labelAbs.Text += ("\n" + absoluteFreq.Values.ElementAt(i));
+                labelRel.Text += ("\n" + relativeFreq.Values.ElementAt(i));
+                labelPerc.Text += ("\n" + percFreq.Values.ElementAt(i));
+            }
+
+        }
+
+        //print all frequency distributions
+        private void printAllMultivariateFrequencyDistributions(Dictionary<string[], double> absoluteFreq, Dictionary<string[], double> relativeFreq, Dictionary<string[], double> percFrequency)
+        {
+
+            //From here I start printing resuslts inside a label on the Windows Form
+            Debug.WriteLine("Multivariate Distribution: ");
+            labelMultivVariables.Text += ("\n\nVARIABLES");
+            labelMultivAbs.Text += ("\n\nABSOLUTE");
+            labelMultivRel.Text += ("\n\nRELATIVE");
+            labelMultiPerc.Text += ("\n\nPERCENTAGE");
+
+            // Print all multivariate distrib.
+            for (int i = 0; i < absoluteFreq.Keys.Count; i++)
+            {
+                // Print the results
+                Debug.WriteLine($"Variables: {string.Join(", ", absoluteFreq.Keys.ElementAt(i))} -> Absolute Frequency: {absoluteFreq.Values.ElementAt(i)}, Relative Frequency: {relativeFreq.Values.ElementAt(i):F2}, Percentage Frequency: {percFrequency.Values.ElementAt(i):F2}%");
+                labelMultivVariables.Text += ("\n" + string.Join(", ", absoluteFreq.Keys.ElementAt(i)));
+                labelMultivAbs.Text += ("\n" + absoluteFreq.Values.ElementAt(i));
+                labelMultivRel.Text += ("\n" + relativeFreq.Values.ElementAt(i));
+                labelMultiPerc.Text += ("\n" + percFrequency.Values.ElementAt(i) + "%");
+            }
+
+        }
+
+    }
+}
